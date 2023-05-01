72eaa1c64fcb971c8c48b78c65e5d6722311b834
decaffjoe <jw3mail+decaff@pm.me>
2023-04-30 23:17:36 -0500

    Add PWA, change calculator icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: [
+        'calculator64.png',
+        'gear.png',
+        'home.png',
+      ],
+      manifest: {
+        name: 'count dracula',
+        short_name: 'count dracula',
+        description: 'Count your calories & protein',
+        theme_color: '#6e6b99',
+        icons: [
+          {
+            src: 'calculator192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'calculator512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      }
+    }),
+  ]
 })
